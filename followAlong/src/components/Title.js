f533c033f905eb42da4a192ecf31fc3ccbb80ca2
jacobplumblambda <jacob.plumb@lambdaschool.com>
2021-08-10 22:40:43 -0400

    August 10, 2021 10:40 PM

--- followAlong/src/components/Title.js
+++ followAlong/src/components/Title.js
@@ -1,20 +1,30 @@
 import React, { useState, useReducer } from 'react';
+import actions from '../action';
+import reducer from '../reducer';
+
+const initalState = {
+  title: 'Default',
+  editing: false,
+  newTitleText: ''
+}
 
 const Title = () => {
+  const [state, dispatch] = useReducer(reducer, initalState);
   const [title, setTitle] = useState('Hello earthlings!');
   const [editing, setEditing] = useState(false);
   const [newTitleText, setNewTitleText] = useState('');
 
   const handleChanges = e => {
+    dispatch(actions.setNewTextTitle(e.target.value));
     setNewTitleText(e.target.value);
   };
 
   return (
     <div>
-      {!editing ? (
+      {!state.editing ? (
         <h1>
           {title}{' '}
-          <i onClick={() => setEditing(!editing)} className="far fa-edit" />
+          <i onClick={() => dispatch(actions.setEditing(true))} className="far fa-edit" />
         </h1>
       ) : (
         <div>
@@ -22,13 +32,13 @@ const Title = () => {
             className="title-input"
             type="text"
             name="newTitleText"
-            value={newTitleText}
+            value={state.newTitleText}
             onChange={handleChanges}
           />
           <button
             onClick={() => {
-              setTitle(newTitleText);
-              setEditing(false);
+              dispatch(actions.setTitle(state.newTitleText))
+              dispatch(actions.setEditing(false));
             }}
           >
             Update title
