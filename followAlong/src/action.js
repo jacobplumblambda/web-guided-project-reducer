6a5dadb9b789a9d02c9cfb1e772ffd342c0146af
jacobplumblambda <jacob.plumb@lambdaschool.com>
2021-08-10 22:28:26 -0400

    August 10, 2021 10:28 PM

--- followAlong/src/action.js
+++ followAlong/src/action.js
@@ -1,1 +1,15 @@
-something1222222
+const SET_TITLE = "SET_TITLE";
+const SET_EDITING = "SET_EDITING";
+const SET_NEWTEXTTITLE = "SET_NEWTEXTTITLE";
+
+export default {
+    setTitle: (title) => {
+        return {type: SET_TITLE, payload: title}
+    },
+    setEditing: (isEditing) => {
+        return {type: SET_EDITING, payload: isEditing}
+    },
+    setNewTextTitle: (newTextTitle) => {
+        return {type: SET_NEWTEXTTITLE, payload: newTextTitle}
+    }
+}
