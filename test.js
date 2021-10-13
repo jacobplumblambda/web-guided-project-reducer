2fa9f75dfb96fea1600c04b4ff04dcb235234c3b
jacobplumblambda <dhagehassan@gmail.com>
2021-10-12 20:05:28 -0400

    updated

--- test.js
+++ test.js
@@ -1,36 +1,36 @@
-// let two = 2;
-// let three = two;
-// two = 4;
+// // let two = 2;
+// // let three = two;
+// // two = 4;
 
 
 
-// console.log(two);
-// console.log(three);
+// // console.log(two);
+// // console.log(three);
 
-const obj1 = {name: 'jacob', age: 30};
-const obj2 = obj1;
-obj2.name = 'james'
+// const obj1 = {name: 'jacob', age: 30};
+// const obj2 = obj1;
+// obj2.name = 'james'
 
-const array1 = [1, 2, 3, 4];
-const array2 = array1;
-array2[1] = 5;
+// const array1 = [1, 2, 3, 4];
+// const array2 = array1;
+// array2[1] = 5;
 
-console.log(array1);
-console.log(array2);
+// console.log(array1);
+// console.log(array2);
 
-const inistalState = {}
+// const inistalState = {}
 
-return makeCoffeeAction = (order) => {
-    return {type: "MAKE COFFEE", order: order}
-}
+// return makeCoffeeAction = (order) => {
+//     return {type: "MAKE COFFEE", order: order}
+// }
 
-const reducer = (state, action) => {
-    switch(action.type) {
-        case "MAKE COFFEE":
-            // setting the cost, the order and updating the status
-            return {...state, order: action.order}
-        default {
-            return {...state}
-        }
-    }
-}
+// const reducer = (state, action) => {
+//     switch(action.type) {
+//         case "MAKE COFFEE":
+//             // setting the cost, the order and updating the status
+//             return {...state, order: action.order}
+//         default {
+//             return {...state}
+//         }
+//     }
+// }
