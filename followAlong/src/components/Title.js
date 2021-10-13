11a63e716a56ef1839610e38df61df446f96952a
jacobplumblambda <dhagehassan@gmail.com>
2021-10-12 20:24:41 -0400

    update

--- followAlong/src/components/Title.js
+++ followAlong/src/components/Title.js
@@ -1,6 +1,26 @@
 import React, { useState, useReducer } from 'react';
 
+const initialState = {
+  title: 'Hello earthlings!',
+  editing: false,
+  newTitleText: ''
+}
+
+const updateTitle = (title) => {
+  return { type: 'UPDATE TITLE', title: title }
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE TITLE':
+      return { ...state, title: action.title }
+    default:
+      return { ...state };
+  }
+}
+
 const Title = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
   const [title, setTitle] = useState('Hello earthlings!');
   const [editing, setEditing] = useState(false);
   const [newTitleText, setNewTitleText] = useState('');
@@ -9,13 +29,20 @@ const Title = () => {
     setNewTitleText(e.target.value);
   };
 
+  const updateTitleDom = () => {
+    updateTitle('new Title')
+  }
+
   return (
     <div>
       {!editing ? (
-        <h1>
-          {title}{' '}
-          <i onClick={() => setEditing(!editing)} className="far fa-edit" />
-        </h1>
+        <div>
+          <h1>
+            {state.title}{' '}
+            <i onClick={() => setEditing(!editing)} className="far fa-edit" />
+          </h1>
+          <button onClick={updateTitleDom}>submit</button>
+        </div>
       ) : (
         <div>
           <input
